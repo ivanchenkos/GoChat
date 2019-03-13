be2ee33b81d15d440b00cb8c50ea9f8631c6355c
HackerIT <40782692+HackerIT@users.noreply.github.com>
2019-03-13 19:48:54 +0500

    Вроде работает, а вроде и нет
    Вроде работает, а вроде и нет

--- newUI/js/js.js
+++ newUI/js/js.js
@@ -1,19 +1,10 @@
+
 $(document).ready(function (){
-		var colors = ['#00a5bd','#347f8a','#4973c7'];
-		var first = 0;
-		var counter = 0;
-		var balls = document.querySelectorAll('.ball');
-		function colorize(){
-			counter = first;
-			first++;
-			if (first>colors.lenght-1) {first=0};
-			balls.forEach(function(ball,n){
-				ball.className='ball';
-				ball.classList.add('ball_'+colors[counter]);
-				counter++;
-				if (counter>colors.lenght-1) {counter=0};
-			});
-			colorize();
-			setInterval(colorize,4000);
-		}
+	var text_length=$('div.subordinate_layer').text().length,
+		height_text = $('#text');
+	$('div.head_lyaer').css('width',height_text.css('width'));	
+	$('div.head_lyaer').css('height',height_text.css('height'));
+	console.log(height_text.css('height'));
+	console.log($('div.head_lyaer').css('height'));
+
 });
