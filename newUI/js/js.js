27c0d82059bbd007ad95e775efccadf8a7927596
HackerIT <40782692+HackerIT@users.noreply.github.com>
2019-03-14 17:00:39 +0500

    Вроде бы работает

--- newUI/js/js.js
+++ newUI/js/js.js
@@ -1,10 +1,6 @@
 
 $(document).ready(function (){
-	var text_length=$('div.subordinate_layer').text().length,
-		height_text = $('#text');
-	$('div.head_lyaer').css('width',height_text.css('width'));	
-	$('div.head_lyaer').css('height',height_text.css('height'));
-	console.log(height_text.css('height'));
-	console.log($('div.head_lyaer').css('height'));
-
+	if ($('.massage').css('height')>$('.massage').css('max-height')) {
+		$('.massage').css('margin-top',-1+"%");
+	}	
 });
