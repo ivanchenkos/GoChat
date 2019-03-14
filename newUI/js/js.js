4f7f0325bc7c52b654109fdaf2c57359ed00f129
HackerIT <40782692+HackerIT@users.noreply.github.com>
2019-03-14 17:53:24 +0500

    Поле ввода автоматически увеличивается в высоте

--- newUI/js/js.js
+++ newUI/js/js.js
@@ -2,5 +2,6 @@
 $(document).ready(function (){
 	if ($('.massage').css('height')>$('.massage').css('max-height')) {
 		$('.massage').css('margin-top',-1+"%");
-	}	
+	}
+	$('div.fon_input').css('height')=$('div.input').css('height');
 });
